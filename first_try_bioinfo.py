88dc3a9955e73d1747ebaec3c4253b56d1c09433
ANAS HASHMI <syedanasalihashmi57@gmail.com>
2024-01-27 19:59:59 +0500

    this is my try to know git hub

---
+++ first_try_bioinfo.py
@@ -0,0 +1,7 @@
+print("hi, this my try to get to know the git hub, push and pop etc")
+print("I will be doing bioinformatics project from zero to high end at which i can go the most")
+print("I only know the basic till know, so we are same")
+print("i will be currently working with the non-secure git hub but later on i will change that")
+
+
+
