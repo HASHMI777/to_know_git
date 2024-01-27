36b03165d4e55e6e1673a4d39a64821dfd32a702
ANAS HASHMI <syedanasalihashmi57@gmail.com>
2024-01-27 20:48:14 +0500

    this is for the push of the github

---
+++ github_push.py
@@ -0,0 +1,3 @@
+print("my previous was try to commit changes in the local repository")
+print("Now we will be going to publish it on the git hub")
+print("I am going to use visual studio code so this is a  bit easy to use as on the cmd")
